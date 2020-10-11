9702e1a89f273a6f14807966432ff5f163b44b39
afnansadid <68118055+afnansadid@users.noreply.github.com>
2020-10-11 15:26:23 +0600

    Update SelectionSortAlgorithm.java

--- SelectionSortAlgorithm.java
+++ SelectionSortAlgorithm.java
@@ -10,15 +10,16 @@ public class SelectionSortAlgorithm {
 }
 
 class SelectionSort {
+
 	int[] selection_sort(int[] dataSet) {
-		for (int i = 0; i < dataSet.length; i++) {
+		for (int i = 0; i < dataSet.length - 1; i++) {
 			int index_min = i; // first index as current minimum value
 			for (int j = index_min + 1; j < dataSet.length; j++) { // Finding minimum value among the rest
 				if (dataSet[j] < dataSet[index_min]) {
 					index_min = j;
 				}
 			}
-			if (dataSet[index_min] <= dataSet[i]) {
+			if (dataSet[index_min] <= dataSet[i]) { // Swiping minimum element with first element
 				int temp = dataSet[i];
 				dataSet[i] = dataSet[index_min];
 				dataSet[index_min] = temp;
